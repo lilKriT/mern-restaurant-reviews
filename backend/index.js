be4ae75c6658cf55147855fc728a7a91ef883515
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-19 23:52:03 +0200

    Working on the backend.

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import app from "./server";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MongoClient = mongodb.MongoClient;
+
+const port = process.env.PORT || 8000;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"; // I can use import because I set this as a module
+import cors from "cors";
+import restaurants from "./api/restaurants.route.js";
+
+// Creating server
+const app = express();
+
+// Apply middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/restaurants", restaurants);
+app.use("*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+export default app;
