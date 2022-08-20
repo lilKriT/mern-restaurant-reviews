f5e18fa9afb28745f82069c9650776dda625b44a
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-20 22:22:46 +0200

    Working on the server.

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,18 @@ dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.MONGO_URI, {
+  poolSize: 50,
+  wtimeout: 2500,
+  useNewUrlParse: true,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
+  });
